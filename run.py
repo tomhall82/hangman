55294229dc2dd5fb3dd848552c3e162057e9c032
tomhall82 <tom.hall82@yahoo.co.uk>
2024-05-19 16:25:35 +0100

    changed guesses and generated words into all uppercase and tweaked text order

--- run.py
+++ run.py
@@ -5,14 +5,13 @@ words = ["test","python", "computer", "code", "institute"]
 
 def welcome():
     print("Welcome to HANGMAN!")
-    print("You have 10 guesses to work out the secret word.")
-    print("Good luck!\n")
+    
 
 """
 Generate random word from list
 """
 def random_word():
-    return random.choice(words)
+    return random.choice(words).upper()
 
 answer = random_word()
 
@@ -20,6 +19,8 @@ def play_game():
     """
     Main game function
     """
+    print("You have 5 lives to work out the secret word.")
+    print("Good luck!\n")
     print(answer)
 
     lives = 5
@@ -31,24 +32,29 @@ def play_game():
         While loop runs until lives hit zero, player wins or
         enters "quit" to return to main menu
         """
-        print(hidden_answer)
         print(f"Lives remaining: {lives}")
-        print(f"Letters guessed: {guesses}")
+        print(f"Letters guessed: {guesses}\n")
+        print(hidden_answer)
 
-        guess = input("Please guess a letter: ")
+        guess = input("\nPlease guess a letter: ").upper()
         if guess in guesses:
             print(f"\nNo no no! You have already tried '{guess}', try again!\n")
         elif guess == "quit":
             break
+        # elif guess == answer:
+        #     hidden_answer = update_hidden_answer(hidden_answer, answer, guess)
+        #     guesses.append(guess)
+        #     print(hidden_answer)
+        #     break
         elif guess in answer:
             hidden_answer = update_hidden_answer(hidden_answer, answer, guess)
             guesses.append(guess)
             # Check to see if player has won
             if hidden_answer == answer:
+                print(f"\n{hidden_answer}")
                 break
             else:
-                print(f"\nWell done! '{guess}' is in the secret word!\n")
-                
+                print(f"\nWell done! '{guess}' is in the secret word!\n")   
         else:
             lives -= 1
             print(f"\nToo bad, '{guess}' is not in the secret word!\n")
@@ -65,10 +71,14 @@ def play_game():
 
     play_again()
 
+# def validate_guess():
+#     # try:
+#     #     if len(guess) == 1 and guess.isalpha():
+            
 
 def play_again():
     try:
-        again = input("would you like to play again? (Y/N) ")
+        again = input("would you like to play again? (Y/N) ").upper()
         again != "Y" or "N"
         if again == "Y":
             print(f"\n\nLETS GO ROUND AGAIN!\n\n")
