125138fc9c158a37e5a559e28a0cf20e7c134811
tomhall82 <tom.hall82@yahoo.co.uk>
2024-05-21 22:31:15 +0100

    added print messages into ascii prints and amended positionings

--- ascii_art.py
+++ ascii_art.py
@@ -1,18 +1,24 @@
 def hangman_title():
     print("""
  
- _   _  ___  _   _ ________  ___ ___  _   _ 
-| | | |/ _ \| \ | |  __ \  \/  |/ _ \| \ | |
-| |_| / /_\ \  \| | |  \/ .  . / /_\ \  \| |
-|  _  |  _  | . ` | | __| |\/| |  _  | . ` |
-| | | | | | | |\  | |_\ \ |  | | | | | |\  |
-\_| |_|_| |_|_| \_/\____|_|  |_|_| |_|_| \_/
+  _   _  ___  _   _ ________  ___ ___  _   _ 
+  | | | |/ _ \| \ | |  __ \  \/  |/ _ \| \ | |
+  | |_| / /_\ \  \| | |  \/ .  . / /_\ \  \| |
+  |  _  |  _  | . ` | | __| |\/| |  _  | . ` |
+  | | | | | | | |\  | |_\ \ |  | | | | | |\  |
+  \_| |_|_| |_|_| \_/\____|_|  |_|_| |_|_| \_/
                                             
+  Welcome to HANGMAN! Try and guess the secret
+word before you run out of lives! Feeling brave?
+        You can gues the whole word!
+But beware... get it wrong and it will cost you...
+          
+             Press enter to play!
 """)
-    print("Welcome to HANGMAN! Try and guess the secret")
-    print("word before you run out of lives! Feeling brave?")
-    print("You can gues the whole word!")
-    print("\nBut beware... get it wrong and it will cost you...")
+    print("")
+    print("")
+    print("")
+    print("\n")
 
 def trophy():
     print(""" 
@@ -28,3 +34,20 @@ def trophy():
                _.' '._
               =========
           """)
+    
+def thanks():
+    print("""
+
+  _____ _  _   _   _  _ _  _____   ___ ___  ___  
+ |_   _| || | /_\ | \| | |/ / __| | __/ _ \| _ \ 
+   | | | __ |/ _ \| .` | ' <\__ \ | _| (_) |   / 
+   |_| |_||_/_/ \_\_|\_|_|\_\___/ |_| \___/|_|_\ 
+         ___ _      ___   _____ _  _  ___ _ 
+        | _ \ |    /_\ \ / /_ _| \| |/ __| |
+        |  _/ |__ / _ \ V / | || .` | (_ |_|
+        |_| |____/_/ \_\_| |___|_|\_|\___(_)
+          
+                See you again soon!
+          
+              Press enter to continue
+""")
